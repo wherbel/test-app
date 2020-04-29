c3a137ad772ff095386c92d9d753d6584fb71d55
wherbel <wilhelm.herbel@ecom-ex.com>
2020-04-29 15:43:07 +0200

    Updates

---
+++ precache-manifest.28c472facfbd38bc7dc1aa25b391539e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0c308b4707bc872964e0a6a4a05acd",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "37dadcafefcd11f87f7f",
+    "url": "/test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23d708970c02d7e3905c",
+    "url": "/test-app/static/js/2.3e4801ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app/static/js/2.3e4801ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dadcafefcd11f87f7f",
+    "url": "/test-app/static/js/main.1865c363.chunk.js"
+  },
+  {
+    "revision": "b3c08ce8953bee725834",
+    "url": "/test-app/static/js/runtime-main.39e027db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
